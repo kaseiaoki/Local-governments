74e18f7cca0281963f12f9daf4ce9af0305d3254
aucuba <aoki.11235813@ezweb.ne.jp>
2020-04-29 02:09:27 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
-    <?php
-    /**
-     * Created by PhpStorm.
-     * User: kaseiaoki
-     * Date: 2020/04/28
-     * Time: 23:14
-     */
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kaseiaoki
+ * Date: 2020/04/28
+ * Time: 23:14
+ */
 
-    require('./vendor/autoload.php');
-    require('./src/client.php');
-    require('./src/jsonParse.php');
+require('./vendor/autoload.php');
+require('./src/client.php');
+require('./src/jsonParse.php');
 
-    $client = new client;
-    $data = $client->basic_request();
-    $jsonParse = new jsonParse($data);
-    $jsonParse->csv();
+/* requst */
+$client = new client;
+$data = $client->basic_request();
+/* parse */
+$jsonParse = new jsonParse($data);
+$jsonParse->csv();
