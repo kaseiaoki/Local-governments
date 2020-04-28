fb3ae1e2ddddc7108759cf0baf7a57a74da614ba
kaseiaoki <aoki070207@gmail.com>
2020-04-29 02:06:47 +0900

    fix

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: kaseiaoki
- * Date: 2020/04/28
- * Time: 23:14
- */
+    <?php
+    /**
+     * Created by PhpStorm.
+     * User: kaseiaoki
+     * Date: 2020/04/28
+     * Time: 23:14
+     */
 
-require('./vendor/autoload.php');
-require('./src/client.php');
-require('./src/jsonParse.php');
+    require('./vendor/autoload.php');
+    require('./src/client.php');
+    require('./src/jsonParse.php');
 
-$client = new client;
-$data = $client->basic_request();
-$jsonParse = new jsonParse($data);
-$jsonParse->csv();
+    $client = new client;
+    $data = $client->basic_request();
+    $jsonParse = new jsonParse($data);
+    $jsonParse->csv();
